5dd359120e5c57b1dbdc4e25492be5c8ca1306ac
ttuannanhh <113655800+ttuannanhh@users.noreply.github.com>
2022-12-12 19:56:06 +0700

    Add files via upload

---
+++ Syntax/src/basic/dev/MainApp.java
@@ -0,0 +1,34 @@
+package basic.dev;
+
+	/**  * Ghi chú (comment nhiều dòng)
+	 *    @author TranThanhBinh  
+	 *  1. Tên lớp trùng tên file 
+	 *  2. Tên lớp chữ cái đầu viết hoa.Nếu từ ghép thì những chữ cái 
+	đầu viết hoa 
+	 *  4. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Peson : lớp 
+	mô tả con người  
+	* - Interface: Phải có tiền tố I. Ví dụ: IListener  
+	* - Abstract: Phải có tiền tố Abstract.Ví dụ: AbstractShape  
+	*/ 
+	public class MainApp { 
+		//Comment 1 dòng cách 1. Phím nóng Ctr + / 
+		/*Comment 1 dòng cách 2*/
+		   public static void main(String[] args) { 
+			System.out.println("Hello World");  
+			/**    * Cách đặt tên biến:    
+			 *  1. Không chứa ký tự đặc biệt !@#$%^&*()-+=   
+			 *  2. Không chứa khoảng trắng    
+			 *  3. Viết thường    
+			 *  4. Không được bắt đầu bằng số   
+			 *   */   
+			int number;   
+			String nameSchool;  
+			}    
+		/*   
+		 * tên phương thức tuân theo quy tắc camelCase  
+	     */  
+		public void show() { 
+		}
+	}
+	 
+
