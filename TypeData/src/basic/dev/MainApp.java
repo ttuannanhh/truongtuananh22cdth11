d80cfd3aef844b697c4a6f335722f5eb57a5325e
ttuannanhh <113655800+ttuannanhh@users.noreply.github.com>
2022-12-12 21:12:51 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp.java
@@ -0,0 +1,47 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		
+		// Thông tin sinh viên
+		
+		String hoVaTen = "Truong Tuan Anh";
+		String maSinhVien = "2260190";
+		String chungMinhNhanDan = "191974182";
+		String tenLop = "22CDTH11";
+		String tenTruong = "Cao dang cong nghiep Hue";
+		System.out.println("Ho va Ten:"+ hoVaTen);
+		System.out.println("Ma sinh vien:"+ maSinhVien);	
+		System.out.println("Chung minh nhan dan:"+ chungMinhNhanDan);
+		System.out.println("Ten lop:"+ tenLop);
+		System.out.println("Ten truong:"+ tenTruong);
+		
+		// Thông tin cong dan
+		
+		String Ten = "Tuan Anh";
+		String diaChi = "Phong Son Phong Dien Thua Thien Hue";
+		String soDienThoai = "0372761116";
+		String heSoLuong = "3 trieu";
+		System.out.println("Ten:"+ Ten);
+		System.out.println("Dia chi:"+ diaChi);
+		System.out.println("So dien thoai:"+ soDienThoai);
+		System.out.println("He so luong:"+ heSoLuong);
+		
+		// Thong tin mot chiec xe car
+		
+		String Ten1 = "Lamborghini Veneno";
+		String Hang = "Lamborghini";
+		String Gia = "60,000,000,000 VND";
+		String mauSon = "Trang den";
+		System.out.println("Ten1:"+ Ten1);
+		System.out.println("Hang:"+ Hang);
+		System.out.println("Gia:"+ Gia);
+		System.out.println("Mau son:"+ mauSon);
+		
+		
+		
+		
+		
+	}
+
+}
