ab921a28dea5475684494729e2f1c543de9029ff
ttuannanhh <113655800+ttuannanhh@users.noreply.github.com>
2022-12-12 19:59:25 +0700

    Add files via upload

---
+++ Variable/src/basic/dev/MainApp.java
@@ -0,0 +1,22 @@
+package basic.dev;
+
+public class MainApp {
+ 
+	public static void main(String[] args) {   
+	/**    
+	 *  Khai báo biến   
+     */   
+             String firstName = "Truong Tuan";   
+			 String lastName = "Anh";   
+			 int old = 15;   
+			 float markMath = 9f;   
+			 boolean passed = true;      
+			 System.out.println("Ho va ten:");   
+			 System.out.println(firstName + " " + lastName);  
+			 System.out.println("Mark Math: " + markMath);   
+			 System.out.println("Passed: " + passed);         
+			 final float PI = 3.14f;  
+			 } 
+		 }
+
+
