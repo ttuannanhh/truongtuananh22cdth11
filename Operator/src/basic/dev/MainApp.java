fe7b833146293aab82a27d391af6cc4b5b27d9f8
ttuannanhh <113655800+ttuannanhh@users.noreply.github.com>
2022-12-14 18:07:22 +0700

    Add files via upload

---
+++ Operator/src/basic/dev/MainApp.java
@@ -0,0 +1,34 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+	public static void main(String[] args) {
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap vao a = ");
+		a = sc.nextInt();
+		System.out.println("Nhap vao b = ");
+		b = sc.nextInt();
+		
+		int tong = a + b;
+		System.out.println(a+" + "+b+" = "+tong);
+		
+		int hieu = a - b;
+		System.out.println(a+" - "+b+" = "+hieu);
+		
+		int nhan = a * b;
+		System.out.println(a+" * "+b+" = "+nhan);
+		
+		float thuong = (float)a / b;
+		System.out.println(a+" / "+b+" = "+thuong);
+		
+		
+
+	 }
+}
+	
+
+
+
+
